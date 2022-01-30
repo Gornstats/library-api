a59b8ac18547537467db892f71892c8d4fd49970
Jeremy Gorniak <railspider@gmail.com>
2022-01-30 21:22:15 +1100

    Added API support with DRF
    Only full book list view

--- library_project/settings.py
+++ library_project/settings.py
@@ -36,8 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # 3rd party
+    'rest_framework',
+    
     # Local
     'books.apps.BooksConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
